3ae1f746032d05913216b25f1a8b5d44e1851e00
tsai <mengjietsai@163.com>
2020-08-01 21:36:33 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>wheel UI</h1>
@@ -15,7 +18,24 @@
   </div>
 </template>
 <style lang="scss" scoped>
-.topnar {
-  border: 1px solid red;
+.topnav {
+  background: rgb(33, 36, 41);
+  color: #fff;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    // 把其他元素挤到右边去
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      font-weight: 600;
+      margin: 0 1em;
+    }
+  }
 }
 </style>

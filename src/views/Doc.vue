35975e3e6c9bf16bef4876e0f836d99237bdf1e1
tsai <mengjietsai@163.com>
2020-08-02 11:12:41 +0800

    Doc layout 布局

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -48,45 +48,39 @@ export default {
     flex-shrink: 0;
   }
   > .content {
+    display: flex;
     flex-grow: 1;
     padding-top: 60px;
     padding-left: 156px;
+    background: #ccc;
     @media (max-width: 500px) {
       padding-left: 0;
     }
-  }
-}
-.content {
-  display: flex;
-  > aside {
-    flex-shrink: 0;
-  }
-  > main {
-    flex-grow: 1;
-    padding: 16px;
-    background: lightgreen;
-  }
-}
-aside {
-  background: rgb(203, 255, 210);
-  width: 150px;
-  padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
-  height: 100%;
-
-  > h2 {
-    margin-bottom: 4px;
-  }
-  > ol {
-    > li {
-      padding: 4px 0;
+    > aside {
+      flex-shrink: 0;
+      position: fixed;
+      top: 0;
+      left: 0;
+      background: rgb(203, 255, 210);
+      width: 150px;
+      padding: 16px;
+      padding-top: 70px;
+      height: 100%;
+      > h2 {
+        margin-bottom: 4px;
+      }
+      > ol {
+        > li {
+          padding: 4px 0;
+        }
+      }
     }
-  }
-  main {
-    overflow: auto;
+    > main {
+      flex-grow: 1;
+      padding: 16px;
+      background: lightgreen;
+      overflow: auto;
+    }
   }
 }
 </style>
